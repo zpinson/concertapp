cf2d7b7ed23095fe1756e4f31df3f1314559bef5
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-04-13 10:41:08 -0400

    corrected the route in index api

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const eventRoutes = require("./events");
 
 // For anything else, render the html page
-router.use("events", eventRoutes);
+router.use("/events", eventRoutes);
 
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../../client/public/index.html"));
