35ac2e4ef558ce566cb30cd2d745f1610810a62d
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-04-14 14:31:53 -0400

    route index.js

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use("*", (req, res) =>
+if (process.env.NODE_ENV === 'production'){
+  router.use("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../client/public/index.html"))
 );
+
+}
 
 module.exports = router;
