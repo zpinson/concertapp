cb6122bc62f94c2beb6b35e17929c132692c9a63
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-04-15 11:15:55 -0400

    changed route index

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 // if (process.env.NODE_ENV === 'production'){
-router.use("*", (req, res) =>
+router.use((req, res) =>
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 
